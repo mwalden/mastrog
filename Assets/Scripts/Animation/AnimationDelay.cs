a405b2a428d5bc243a89d439e7b9c77ea372646e
mwalden <tont0r@hotmail.com>
2018-01-10 17:19:17 -0500

    added a bunch of art.

---
+++ Assets/Scripts/Animation/AnimationDelay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationDelay : MonoBehaviour {
+
+	public string animationName;
+	public float delay;
+	private Animation animator;
+	void Start () {
+		animator = GetComponent<Animation>();
+
+
+	}
+
+}
